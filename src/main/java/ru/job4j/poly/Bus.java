5c31b470b7e28423746cac4ad1828974093facbe
Aleksandr Semeikin <sensem@yandex.ru>
2022-08-20 18:03:29 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,22 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    private double pricePerLitre = 5.05;
+
+    @Override
+    public void ride() {
+        System.out.println("Let's go");
+    }
+
+    @Override
+    public void takePassengers(int number) {
+        if (number > 100) {
+            System.out.println("Rush hour");
+        }
+    }
+
+    @Override
+    public double fuel(double quantity) {
+        return pricePerLitre * quantity;
+    }
+}
