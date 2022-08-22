c870ec3af52d35eaaf2f86e02174c23d01703a1d
Aleksandr Semeikin <sensem@yandex.ru>
2022-08-22 16:42:23 +0300

    5. Input и полиморфизм. [#181094] Фиксация изменений

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,16 +8,12 @@ public class StartUITest {
 
     @Test
     public void whenAddItem() {
-        String[] answers = {"Fix PC", "Upgrade PC"};
+        String[] answers = {"Fix PC"};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
         StartUI.createItem(input, tracker);
-        Item created1 = tracker.findAll()[0];
-        Item expected1 = new Item("Fix PC");
-        StartUI.createItem(input, tracker);
-        Item created2 = tracker.findAll()[1];
-        Item expected2 = new Item("Upgrade PC");
-        assertThat(created1.getName()).isEqualTo(expected1.getName());
-        assertThat(created2.getName()).isEqualTo(expected2.getName());
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName()).isEqualTo(expected.getName());
     }
 }
