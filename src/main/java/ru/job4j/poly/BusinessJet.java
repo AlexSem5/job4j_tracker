dbab6e985d022a615809ae429a94da4605c95e12
Aleksandr Semeikin <sensem@yandex.ru>
2022-08-25 01:56:44 +0300

    Interfaces Update New

---
+++ src/main/java/ru/job4j/poly/BusinessJet.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class BusinessJet implements Transport {
+    @Override
+    public void ride() {
+        System.out.println("hi");
+    }
+
+    @Override
+    public void takePassengers(int number) {
+
+    }
+
+    @Override
+    public double fuel(double quantity) {
+        return 0;
+    }
+}
