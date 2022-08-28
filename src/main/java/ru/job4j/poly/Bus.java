cdee7948e64f0ea0c36c69b27a41e710d354f779
Aleksandr Semeikin <sensem@yandex.ru>
2022-08-28 19:43:26 +0300

    new

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,7 +1,7 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport {
-    private double pricePerLitre = 5.07;
+    private double pricePerLitre = 5.5;
 
     @Override
     public void ride() {
