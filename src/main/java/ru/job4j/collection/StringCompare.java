509dd9ef7cb1fa330381827b94bb463e0a850c4a
Aleksandr Semeikin <sensem@yandex.ru>
2022-09-14 14:24:26 +0300

    3. Компаратор для строк [#35008 #343052] Фиксация изменений

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -8,32 +8,14 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
         int result = 0;
-        int compareLength = Integer.compare(o1.length(), o2.length());
-        if (compareLength < 0) {
-            for (int i = 0; i < o1.length() - 1; i++) {
-                if (o1.charAt(i) != o2.charAt(i)) {
-                    result = o1.charAt(i) - o2.charAt(i);
-                    break;
-                }
-                result = compareLength;
+        int minLength = Math.min(o1.length(), o2.length());
+        for (int i = 0; i < minLength - 1; i++) {
+            int compare = Character.compare(o1.charAt(i), o2.charAt(i));
+            if (compare != 0) {
+                result = compare;
+                break;
             }
-        }
-        if (compareLength > 0) {
-            for (int i = 0; i < o2.length() - 1; i++) {
-                if (o1.charAt(i) != o2.charAt(i)) {
-                    result = o1.charAt(i) - o2.charAt(i);
-                    break;
-                }
-                result = compareLength;
-            }
-        }
-        if (compareLength == 0) {
-            for (int i = 0; i < o2.length() - 1; i++) {
-                if (o1.charAt(i) != o2.charAt(i)) {
-                    result = o1.charAt(i) - o2.charAt(i);
-                    break;
-                }
-            }
+            result = Integer.compare(o1.length(), o2.length());
         }
         return result;
     }
