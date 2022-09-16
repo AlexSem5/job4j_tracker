5625e3d6201354b963dcea1a0d1b02cf9472d104
Aleksandr Semeikin <sensem@yandex.ru>
2022-09-17 02:19:23 +0300

    2. Отсортировать департаменты [#34352] Фиксация изменений

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -11,11 +11,6 @@ public class DepDescComp implements Comparator<String> {
         if (!left[0].equals(right[0])) {
             return right[0].compareTo(left[0]);
         }
-
-        if (left.length == right.length) {
-            int diff = left[1].compareTo(right[1]);
-            return diff != 0 ? diff : left[2].compareTo(right[2]);
-        }
-        return Integer.compare(left.length, right.length);
+        return o1.compareTo(o2);
     }
 }
