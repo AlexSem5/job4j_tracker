5e7d31f1276363e06da5a12499a112ba8377d427
Aleksandr Semeikin <sensem@yandex.ru>
2022-12-15 14:53:41 +0300

    2. Трекер SQL [#1734 #371116] Фиксация изменений

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.sql.*;
@@ -8,6 +11,9 @@ import java.util.List;
 import java.util.Properties;
 
 public class SqlTracker implements Store {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(SqlTracker.class.getName());
+    
     private Connection cn;
     
     public SqlTracker() {
@@ -29,7 +35,7 @@ public class SqlTracker implements Store {
                     config.getProperty("password")
             );
         } catch (Exception e) {
-            throw new IllegalStateException(e);
+            LOG.error("Exception in SQLTracker", e);
         }
     }
     
@@ -45,7 +51,8 @@ public class SqlTracker implements Store {
     Они выполняют указанный при создании объекта (PreparedStatement) SQL-запрос с подставленными аргументами*/
     @Override
     public Item add(Item item) {
-        try (PreparedStatement ps = cn.prepareStatement("INSERT INTO items (name, created) VALUES (?,?)")) {
+        try (PreparedStatement ps = cn.prepareStatement("INSERT INTO items (name, created) VALUES (?,?);",
+                Statement.RETURN_GENERATED_KEYS)) {
             /* Из Timestamp в LocalDateTime */
             /*long millis = System.currentTimeMillis();
             Timestamp timestamp = new Timestamp(millis);
@@ -55,21 +62,29 @@ public class SqlTracker implements Store {
             ps.setString(1, item.getName());
             ps.setTimestamp(2, timestampFromLDT);
             ps.execute();
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    item.setId(generatedKeys.getInt(1));
+                }
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOG.error("Exception in SQLTracker", e);
         }
         return item;
     }
     
     @Override
     public boolean replace(int id, Item item) {
-        boolean result;
-        try (PreparedStatement ps = cn.prepareStatement("UPDATE items SET name = ? WHERE id = ?")) {
+        boolean result = false;
+        try (PreparedStatement ps = cn.prepareStatement(
+                "UPDATE items SET name = ?, created = ? WHERE id = ?;")) {
             ps.setString(1, item.getName());
-            ps.setInt(2, id);
+            Timestamp timestampFromLDT = Timestamp.valueOf(item.getCreated());
+            ps.setTimestamp(2, timestampFromLDT);
+            ps.setInt(3, id);
             result = ps.executeUpdate() > 0;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            LOG.error("Exception in SQLTracker", e);
         }
         return result;
     }
@@ -77,30 +92,40 @@ public class SqlTracker implements Store {
     @Override
     public boolean delete(int id) {
         boolean result = false;
-        try (PreparedStatement ps = cn.prepareStatement("DELETE FROM items WHERE id = ?")) {
+        try (PreparedStatement ps = cn.prepareStatement("DELETE FROM items WHERE id = ?;")) {
             ps.setInt(1, id);
             result = ps.executeUpdate() > 0;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            LOG.error("Exception in SQLTracker", e);
         }
         return result;
+    }
+    
+    private Item createItem(ResultSet resultSet) {
+        Item item = null;
+        try {
+            item = new Item(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getTimestamp("created").toLocalDateTime()
+            );
+        } catch (SQLException e) {
+            LOG.error("Exception in SQLTracker", e);
+        }
+        return item;
     }
     
     @Override
     public List<Item> findAll() {
         List<Item> items = new ArrayList<>();
-        try (PreparedStatement statement = cn.prepareStatement("SELECT * FROM items")) {
+        try (PreparedStatement statement = cn.prepareStatement("SELECT * FROM items;")) {
             try (ResultSet resultSet = statement.executeQuery()) {
                 while (resultSet.next()) {
-                    items.add(new Item(
-                            resultSet.getInt("id"),
-                            resultSet.getString("name"),
-                            resultSet.getTimestamp("created").toLocalDateTime()
-                    ));
+                    items.add(createItem(resultSet));
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.error("Exception in SQLTracker", e);
         }
         return items;
     }
@@ -108,19 +133,15 @@ public class SqlTracker implements Store {
     @Override
     public List<Item> findByName(String key) {
         List<Item> items = new ArrayList<>();
-        try (PreparedStatement statement = cn.prepareStatement("SELECT * FROM items WHERE name = ?")) {
+        try (PreparedStatement statement = cn.prepareStatement("SELECT * FROM items WHERE name = ?;")) {
             statement.setString(1, key);
             try (ResultSet resultSet = statement.executeQuery()) {
                 while (resultSet.next()) {
-                    items.add(new Item(
-                            resultSet.getInt("id"),
-                            resultSet.getString("name"),
-                            resultSet.getTimestamp("created").toLocalDateTime()
-                    ));
+                    items.add(createItem(resultSet));
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.error("Exception in SQLTracker", e);
         }
         return items;
     }
@@ -128,19 +149,15 @@ public class SqlTracker implements Store {
     @Override
     public Item findById(int id) {
         Item item = null;
-        try (PreparedStatement statement = cn.prepareStatement("SELECT * FROM items WHERE id = ?")) {
+        try (PreparedStatement statement = cn.prepareStatement("SELECT * FROM items WHERE id = ?;")) {
             statement.setInt(1, id);
             try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    item = new Item(
-                            resultSet.getInt("id"),
-                            resultSet.getString("name"),
-                            resultSet.getTimestamp("created").toLocalDateTime()
-                    );
+                if (resultSet.next()) {
+                    item = createItem(resultSet);
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.error("Exception in SQLTracker", e);
         }
         return item;
     }
